d25758cbeda539f31c61123fdb02795965599170
adipadi12 <ajha4436@gmail.com>
2025-03-25 23:29:06 +0530

    button turns yellow through code lessfuckingooooo

---
+++ Platformer/Assets/Workflow/ButtonScreen.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+//using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ButtonScreen : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        UIDocument ui = GetComponent<UIDocument>();
+        VisualElement root = ui.rootVisualElement;
+
+        Button button = root.Q<Button>("MyButton");
+        button.RegisterCallback<ClickEvent>(elem => button.style.backgroundColor = Color.yellow);
+        Debug.Log
+            ("Nigga");
+    }
+}
