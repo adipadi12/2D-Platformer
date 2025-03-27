c0e8a33312c3aa266a277c6a2066a52d8a5283ed
adipadi12 <ajha4436@gmail.com>
2025-03-27 17:19:15 +0530

    made changes to label through code in update loop

--- Platformer/Assets/ProjSetup4/ButtonScreen.cs
+++ Platformer/Assets/ProjSetup4/ButtonScreen.cs
@@ -5,24 +5,21 @@ using UnityEngine.UIElements;
 
 public class ButtonScreen : MonoBehaviour
 {
-    VisualElement box;
-    Button myButton;
+    Label label;
+
     private void OnEnable()
     {
-        UIDocument ui = GetComponent<UIDocument>();
-        VisualElement root = ui.rootVisualElement; //accesses the root inside the UXML file
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        box = root.Q("Box"); //gets references to element in our layout
-        myButton = root.Q<Button>("MyButton");
-
-        myButton.clicked += OnButtonClicked; //subscribing to this
+        label = root.Q<Label>("MyLabel");
     }
 
-    void OnButtonClicked()
-    {
-        int randomWidth = Random.Range(50, 300);
 
-        box.style.width = randomWidth; //getting the style to randomize on button being clicked
-        box.style.height = randomWidth;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            label.style.backgroundColor = Color.black;
+        }
     }
 }
