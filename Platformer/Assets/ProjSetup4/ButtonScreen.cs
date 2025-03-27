9665396a042075c7a8ed4390447859dc3fbca3bd
adipadi12 <ajha4436@gmail.com>
2025-03-27 21:50:51 +0530

    slider shit man some shi w scripts and templates done

--- Platformer/Assets/ProjSetup4/ButtonScreen.cs
+++ Platformer/Assets/ProjSetup4/ButtonScreen.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class ButtonScreen : MonoBehaviour
 {
     VisualElement element;
+    VisualElement child;
     Slider slider;
 
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        element = root.Q("Box");
+        // element = root.Q("Box");
+        element = root.Q("Container");
         slider = root.Q<Slider>("MySlider");
 
         slider.RegisterCallback<ChangeEvent<float>>(OnSliderChanged); //subscribing to this event
+
+
+        VisualTreeAsset template = Resources.Load<VisualTreeAsset>("Box");
+
+        VisualElement card = template.Instantiate();
+        element.Add(card);
+
+        //loasding image
+        Sprite img = Resources.Load<Sprite>("img/nature bg.png");
+        VisualElement avatar = card.Q("Avatar");
+        avatar.style.backgroundImage = new StyleBackground(img);
     }
 
     void OnSliderChanged(ChangeEvent<float> evt)
     {
         element.style.width = evt.newValue; //takes the float of the slider value as an argument and passes into width field to alter
+        element.Add(child);
     }
-    
+
 }
